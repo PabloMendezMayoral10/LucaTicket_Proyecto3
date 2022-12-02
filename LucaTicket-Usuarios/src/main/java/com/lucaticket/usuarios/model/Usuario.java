308b3addd1ef5a42b0914ed75f57b79c3ced5608
alvaroblancosangines <alvaroblancones@gmail.com>
2022-12-02 10:35:29 +0100

    usuarios

---
+++ LucaTicket-Usuarios/src/main/java/com/lucaticket/usuarios/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.lucaticket.usuarios.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Setter @Getter
+public class Usuario {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username, password,email;
+	//private List<Evento> eventos;
+	
+
+}
---
+++ LucaTicket-Usuarios/src/test/java/com/lucaticket/usuarios/LucaTicketUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaticket.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaTicketUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
