105c63e01f4c54bf10395c9c7035af303994e681
Pedro Pusso <pedropussomuro@gmail.com>
2022-12-02 14:02:24 +0100

    menudo lio

---
+++ LucaTicket-Usuarios/src/main/java/com/lucaticket/response/UsuarioResponse.java
@@ -0,0 +1,12 @@
+package com.lucaticket.response;
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsuarioResponse implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private static Logger log = LoggerFactory.getLogger(UsuarioResponse.class);
+
+}
