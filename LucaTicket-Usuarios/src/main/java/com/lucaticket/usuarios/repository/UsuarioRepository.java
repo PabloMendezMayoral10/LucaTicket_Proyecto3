2f9751707bdfeeb0445315b7b5285c20a6a3f656
unknown <domeofvermia@outlook.com>
2022-12-02 10:45:55 +0100

    usuarioRepository

---
+++ LucaTicket-Usuarios/src/main/java/com/lucaticket/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.lucaticket.usuarios.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucaticket.usuarios.model.Usuario;
+
+/**
+ * UsuarioRepository
+ * @author Martin
+ * Interfaz para conectar con la base de datos de usuarios
+ * @version 1.0
+ * 02-12-2022
+ */
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
--- com.lucaticket.eureka/src/main/java/com/lucaticket/eureka/Application.java
+++ com.lucaticket.eureka/src/main/java/com/lucaticket/eureka/Application.java
@@ -3,7 +3,13 @@ package com.lucaticket.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * Application
+ * @author Martin
+ * Servidor Eureka para el proyecto LucaTicket
+ * @version 1.0
+ * 02-12-2022
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
