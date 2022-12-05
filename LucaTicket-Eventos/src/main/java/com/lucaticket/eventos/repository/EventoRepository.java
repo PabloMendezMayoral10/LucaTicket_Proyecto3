53402d81adeab3981cd952b9f706780cac8baede
unknown <domeofvermia@outlook.com>
2022-12-05 18:34:09 +0100

    VACIO

--- LucaTicket-Eventos/src/main/java/com/lucaticket/eventos/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lucaticket.eventos.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.lucaticket.eventos.model.Evento;
-
-public interface EventoRepository extends MongoRepository<Evento,String>{
-
-}
