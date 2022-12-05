9863a0f301326c7d54c018df864a8e2301761e5b
Ioan Stirbu <55089021+IOANSTIRBU@users.noreply.github.com>
2022-12-05 17:41:06 +0100

    testAltaEventoExiste

--- LucaTicket-Eventos/src/test/java/com/lucaticket/eventos/LucaTicketEventosApplicationTests.java
+++ LucaTicket-Eventos/src/test/java/com/lucaticket/eventos/LucaTicketEventosApplicationTests.java
@@ -1,13 +1,68 @@
 package com.lucaticket.eventos;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lucaticket.eventos.adapter.EventoAdapter;
+import com.lucaticket.eventos.dto.EventoDTO;
+import com.lucaticket.eventos.model.Evento;
+import com.lucaticket.eventos.repository.EventoRepository;
+
+
 
 @SpringBootTest
 class LucaTicketEventosApplicationTests {
 
+	@Autowired
+	private EventoRepository repository;
+	@Autowired
+	private EventoAdapter adapter;
+	
+	//Comprobar que después de hacer el alta el evento existe
+	
+	/**
+	 * LucaTicketEventossApplicationTests
+	 * @author Ioan y Pablo
+	 * @version 1.0
+	 * 05-12-2022
+	 */
+	
 	@Test
-	void contextLoads() {
+	void testAltaEventoExiste() {
+		List<Evento> eventos = repository.findAll();
+		eventos.add(new Evento(2,"pepe", "senior mayor","un senior muy mayor",
+				"C:\\Desktop",20,300,"No molestar a la gente", "Pepeland",
+				"Madrid","Avenida Pepelandia", "demasiado grande", 6000));
+		
+		List<EventoDTO> edto = adapter.convertToDTO(eventos);
+		
+		for(EventoDTO events : edto) {
+			if(events.getId() == 2) {
+				assertThat(edto).isNotNull();
+			}
+		}
+		
 	}
+	
+	/*List<Usuario> usuarios = repo.findAll();
+
+		usuarios.add(new Usuario(2, "Pablo", "Mendez", "12345", "hola@gmail", new Date(05,12,2022)));
+
+		
+		List<UsuarioDTO> usdto = adapter.of(usuarios);
+		
+		for(UsuarioDTO users : usdto) {
+			if(users.getId() == 2) {
+				assertThat(users).isNotNull();
+			}
+		}
+	 * 
+	 */
 
 }
