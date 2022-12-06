794bc8aed225bc5c2fc9cd04cae9f56fbefdc903
unknown <domeofvermia@outlook.com>
2022-12-06 17:52:15 +0100

    compra no esta?

--- LucaTicket-Compras/src/main/java/com/lucaticket/compras/adapter/CompraAdapter.java
+++ LucaTicket-Compras/src/main/java/com/lucaticket/compras/adapter/CompraAdapter.java
@@ -1,5 +1,20 @@
 package com.lucaticket.compras.adapter;
 
-public class CompraAdapter {
+import com.lucaticket.compras.dto.CompraDTO;
+import com.lucaticket.compras.model.Compra;
 
+/**
+ * CompraAdapter
+ * @author Martin
+ * @since 06-12-2022
+ * Clase que transforma una Compra en su version DTO
+ */
+public class CompraAdapter {
+	public CompraDTO convertToDTO( Compra c ) {
+		CompraDTO dto = new CompraDTO();
+		
+		
+		
+		return dto;
+	}
 }
