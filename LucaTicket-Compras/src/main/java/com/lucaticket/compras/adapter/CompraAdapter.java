e2773f40d6cec6b79f08e8a915ceb1a2154636ce
unknown <domeofvermia@outlook.com>
2022-12-06 18:04:07 +0100

    convertToDTO

--- LucaTicket-Compras/src/main/java/com/lucaticket/compras/adapter/CompraAdapter.java
+++ LucaTicket-Compras/src/main/java/com/lucaticket/compras/adapter/CompraAdapter.java
@@ -1,5 +1,8 @@
 package com.lucaticket.compras.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.lucaticket.compras.dto.CompraDTO;
 import com.lucaticket.compras.model.Compra;
 
@@ -7,14 +10,39 @@ import com.lucaticket.compras.model.Compra;
  * CompraAdapter
  * @author Martin
  * @since 06-12-2022
- * Clase que transforma una Compra en su version DTO
+ * Clase que transforma Compra en su version DTO
  */
 public class CompraAdapter {
+	
+	/**
+	 * convertToDTO
+	 * @author Martin
+	 * @param compra a convertir
+	 * @return la misma compra pero en DTO
+	 * @since 06-12-2022
+	 */
 	public CompraDTO convertToDTO( Compra c ) {
 		CompraDTO dto = new CompraDTO();
 		
-		
+		dto.setId_compra(c.getId_compra());
+		dto.setId_usuario(c.getId_usuario());
+		dto.setId_evento(c.getId_evento());
 		
 		return dto;
+	}
+	
+	/**
+	 * convertToDTO
+	 * @author Martin
+	 * @param lista de compras a convertir
+	 * @return una lista con todas las compras transformadas a DTO
+	 * @since 06-12-2022
+	 */
+	public List<CompraDTO> convertToDTO( List<Compra> compras ) {
+		ArrayList<CompraDTO> dtos = new ArrayList<CompraDTO>();
+		for(Compra c : compras) {
+			dtos.add( convertToDTO(c) );
+		}
+		return dtos;
 	}
 }
