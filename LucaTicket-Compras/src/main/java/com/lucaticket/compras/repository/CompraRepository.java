b9ff64a72e7808724da9c7c28337a6369d70d3c1
Pedro Pusso <pedropussomuro@gmail.com>
2022-12-06 18:08:24 +0100

    interface CompraRepository creada

--- LucaTicket-Compras/src/main/java/com/lucaticket/compras/repository/CompraRepository.java
+++ LucaTicket-Compras/src/main/java/com/lucaticket/compras/repository/CompraRepository.java
@@ -1,5 +1,9 @@
 package com.lucaticket.compras.repository;
 
-public class CompraRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucaticket.compras.model.Compra;
+
+public interface CompraRepository extends JpaRepository<Compra, Integer>{
 
 }
