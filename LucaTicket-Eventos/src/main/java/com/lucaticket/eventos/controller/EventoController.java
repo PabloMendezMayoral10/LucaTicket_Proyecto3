b06841f16e9a4d087a5f8909ffeff2da3789891b
unknown <domeofvermia@outlook.com>
2022-12-06 11:12:26 +0100

    infraestructura mensajes personalizados

--- LucaTicket-Eventos/src/main/java/com/lucaticket/eventos/controller/EventoController.java
+++ LucaTicket-Eventos/src/main/java/com/lucaticket/eventos/controller/EventoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -50,9 +51,8 @@ public class EventoController {
 					@Content(mediaType = "application/json", schema = @Schema(implementation = EventoDTO.class)) }),
 			@ApiResponse(responseCode = "400", description = "", content = @Content)})*/
 	@PostMapping()
-	public ResponseEntity<EventoDTO> save( Evento evento ) {
+	public EventoDTO save(@RequestBody Evento evento ) {
 		EventoDTO respuesta = service.save(evento);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(respuesta.getId()).toUri();
-		return ResponseEntity.created(location).build();
+		return respuesta;
 	}
 }
