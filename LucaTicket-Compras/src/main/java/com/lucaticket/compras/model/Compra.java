12c247db00e2e14adb0b7591ec8fc807de469811
alvaroblancosangines <alvaroblancones@gmail.com>
2022-12-06 17:40:11 +0100

    Identidad Compra realizada

--- LucaTicket-Compras/src/main/java/com/lucaticket/compras/model/Compra.java
+++ LucaTicket-Compras/src/main/java/com/lucaticket/compras/model/Compra.java
@@ -1,5 +1,30 @@
 package com.lucaticket.compras.model;
 
-public class Compra {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "compra")
+@Setter
+@Getter
+//@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Compra {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_compra;
+	private int id_usuario;
+	private String id_evento;
+	
 }
